42bb7a99115852e6e3de97dc5f704a017fd8c08c
Pedro Oliveira <pedro96h@gmail.com>
2023-07-11 07:56:53 -0300

    adicionando logica de passagem de parametro

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
+//  Ativação de menu //
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link){
     const url = window.location.href;
-    const href = link.href;
+    const href = '/'+link.href.split('/')[3].split('.')[0];
     if(url.includes(href)) {
         link.classList.add('ativo');
     }
 };
 
 links.forEach(ativarLink);
+
+// Passagem de parametros na url //
+
+const parametros = new URLSearchParams(window.location.search);
+
+function passagemParametro(parametro){
+    // const element = document.querySelector('#seguro');
+    const element = document.getElementById(parametro);
+    if(element) {
+        element.checked = true;
+    }
+}
+
+parametros.forEach(passagemParametro);
