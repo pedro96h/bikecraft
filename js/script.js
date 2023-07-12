89fb1413dc3af07f84152da738710286f59eb46a
Pedro Oliveira <pedro96h@gmail.com>
2023-07-11 23:28:22 -0300

    adicionando logica a galeria

--- js/script.js
+++ js/script.js
@@ -44,3 +44,22 @@ function eventoPergunta (pergunta) {
 };
 
 perguntas.forEach(eventoPergunta);
+
+// biblioteca de img //
+
+const galeria = document.querySelectorAll('.bicicleta-imagem img');
+const galeriaContainer = document.querySelector('.bicicleta-imagem');
+
+function trocarImagem (event) {
+    const img = event.currentTarget;
+    const media = window.matchMedia('( min-width : 1000px )').matches;
+    if(media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventoGaleria (img) {
+    img.addEventListener('click',trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
